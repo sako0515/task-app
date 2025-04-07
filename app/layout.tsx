08d2ba92f1a52fd5d3a905bbb8b78f337e471474
sako <koshi051511@icloud.com>
2025-04-07 14:27:50 +0900

    💄 新しくスタイリングしました。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 
 export default function RootLayout({
   children,
@@ -6,9 +7,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>
-        <header>タスク管理アプリ</header>
-        <main>{children}</main>
+      <body className="m-0 min-h-screen">
+        <header className="m-[10px] h-[64px] bg-slate-50 rounded-lg">
+          <h1 className="pt-[20px] font-bold">タスク管理アプリ</h1>
+        </header>
+        <div className="flex min-h-screen">
+          <aside className="m-[10px] w-[150px] bg-slate-100 rounded-lg">
+            <nav className="flex pt-[20px]">
+              
+            </nav>
+          </aside>
+          <main className="flex-1 m-[10px]">{children}</main>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -18,35 +18,39 @@ export default function Home() {
       { id: Date.now(), title: newTask, status: "ToDo" },
     ]);
     setNewTask("");
-  }, [newTask]); 
+  }, [newTask]);
+
+  const updateStatus = useCallback((id: number, newStatus: Task["status"]) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === id ? { ...prevTask, status: newStatus } : prevTask
+      )
+    );
+  }, []);
 
   return (
-    <div>
-      <h1>タスク管理アプリ</h1>
-      <input value={newTask} onChange={(e) => setNewTask(e.target.value)} />
-      <button onClick={addTask}>追加</button>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <div>{task.title}</div>
-            <select
-              value={task.status}
-              onChange={(e) =>
-                setTasks(
-                  tasks.map((t) =>
-                    t.id === task.id
-                      ? { ...t, status: e.target.value as Task["status"] }
-                      : t
-                  )
-                )
-              }
-            >
-              <option value="ToDo">ToDo</option>
-              <option value="InProgress">In Progress</option>
-              <option value="Done">Done</option>
-            </select>
-          </li>
-        ))}
+    <div className="flex flex-col bg-slate-200 rounded-lg">
+      <h1 className="m-[16px] font-bold">Todoリスト</h1>
+      <div className="flex m-[16px]">
+        <input className="pl-[6px]" placeholder="入力してください" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+        <button className="ml-[12px]" onClick={addTask}>+</button>
+      </div>
+      <ul className="flex w-[100%] ">
+          {tasks.map((task) => (
+            <li className="bg-slate-300 rounded-lg m-[16px] h-[160px]" key={task.id}>
+              <select
+                value={task.status}
+                onChange={(e) =>
+                  updateStatus(task.id, e.target.value as Task["status"])
+                }
+              >
+                <option value="ToDo">ToDo</option>
+                <option value="InProgress">In Progress</option>
+                <option value="Done">Done</option>
+              </select>
+              <div>{task.title}</div>
+            </li>
+          ))}
       </ul>
     </div>
   );
